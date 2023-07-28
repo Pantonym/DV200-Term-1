4daeb464dcd0e4cc1cdf16044c0140d5c7d2336e
Pantonym <221179@virtualwindow.co.za>
2023-07-28 20:19:44 +0200

    navbar fix

--- term1/src/components/navbar.js
+++ term1/src/components/navbar.js
@@ -4,12 +4,12 @@ import '../App.css';
 function BasicNavbar() {
     return (
 
-        <div className="nav_bg" style={{position: 'fixed', top: 20}}>
+        <div className="nav_bg" style={{ position: 'fixed', top: 20 }}>
 
             <div className="nav flex-column">
-                <a className="nav-link Libre Font_Body" style={{color: 'black'}} href="/">Landing</a>
-                <a className="nav-link Libre Font_Body" style={{color: 'black'}} href="/compare">Compare</a>
-                <a className="nav-link Libre Font_Body" style={{color: 'black'}} href="/timeline">Timeline</a>
+                <a className="nav-link Libre Font_Body" style={{ color: 'black' }} href="/">Landing</a>
+                <a className="nav-link Libre Font_Body" style={{ color: 'black' }} href="/compare">Compare</a>
+                <a className="nav-link Libre Font_Body" style={{ color: 'black' }} href="/timeline">Timeline</a>
             </div>
 
             <hr></hr>
